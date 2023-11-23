0c894b4cff8d310b277ce36e1df847d8a27b8f3d
hoangdbk03 <hoangdbk03@gmail.com>
2023-11-23 20:35:22 +0700

    Thêm chức năng thêm sửa xóa món ăn

--- src/service/initRouter.js
+++ src/service/initRouter.js
@@ -7,17 +7,23 @@ import GioHangController from '../controller/GioHangController'
 const initRouter = (app) => {
   router.get("/user", userController.getUser);
 
-  //lấy mon an
+  // thêm món ăn mới
+  router.post("/addFood", MonAnController.addFood);
+  // cập nhật món ăn theo id
+  router.put("/updateFood/:id", MonAnController.updateFood);
+  // xóa món ăn theo id
+  router.delete("/deleteFood/:id", MonAnController.deleteFood);
+  // lấy mon an
   router.get("/monan", MonAnController.getAllFood);
-  //lấy món ăn theo thể loại
+  // lấy món ăn theo thể loại
   router.get("/monanType/:type", MonAnController.getFoodByType);
-  //lấy món ăn theo id
+  // lấy món ăn theo id
   router.get("/monanId/:id", MonAnController.getFoodById);
-  //lấy đánh giá
+  // lấy đánh giá
   router.get("/rating", MonAnController.getRating);
-  //thêm món ăn vào giỏ hàng
+  // thêm món ăn vào giỏ hàng
   router.post("/addToCart", MonAnController.addToCart);
-  //Lấy thông tin món ăn
+  // Lấy thông tin món ăn
   router.get("/getInfor",GioHangController.getInfor);
 
   return app.use("/", router);
