fd6dec7da9eb4b30850cca6afd23d908499d9fd4
DoannNguyen <nguyendoan27022001@gmail.com>
2023-12-07 13:47:18 +0700

    doan commit

--- src/controller/GioHangController.js
+++ src/controller/GioHangController.js
@@ -2,9 +2,12 @@ import connection from "../config/connecttion";
 
 // lấy thông tin món ăn
 const getInfor = (req, res) => {
-  console.log("get infor");
+  const email = req.params.email;
+  console.log("get infor" + email);
   connection.query(
-    "select a.soLuong, b.anh, b.ten, b.giaBan from giohang_monan a join monan b on a.idMonAn = b.idMonAn",
+    "select a.soLuong, b.anh, b.ten, b.giaBan from giohang_monan a join monan b on a.idMonAn = b.idMonAn join giohang c on a.idGioHang = c.idGioHang " +
+      "join nguoiDung d on c.idNguoiDung = d.idNguoiDung where d.email = ? and c.trangThai = 0",
+    [email],
     (err, result) => {
       if (err) throw err;
       return res.send(result);
